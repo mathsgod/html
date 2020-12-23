c0ee7459aeef468e988b48b8f4bf3e7774c9f6c5
Raymond Chong <mathsgod@yahoo.com>
2020-12-23 12:56:57 +0800

    script not close tag

--- src/Node.php
+++ src/Node.php
@@ -5,7 +5,7 @@ namespace HTML;
 
 class Node
 {
-    const CLOSE_TAG = ["input", "script"];
+    const CLOSE_TAG = ["input"];
     const NO_CLOSE_TAG = ["meta", "link"];
     //const HTML_TAG_LIST=["a","abbr","address","area","article","aside","audio","b","base","bdi","bdo","blockquote","body","br","button","canvas","caption","cite","code","col","colgroup","command","datalist","dd","del","details","dfn","div","dl","dt","em","embed","fieldset","figcaption","figure","footer","form","h1","h2","h3","h4","h5","h6","head","header","hgroup","hr","html","i","iframe","img","input","ins","kbd","keygen","label","legend","li","link","map","mark","menu","meta","meter","nav","noscript","object","ol","optgroup","option","output","p","param","pre","progress","q","rp","rt","ruby","s","samp","script","section","select","small","source","span","strong","style","sub","summary","sup","table","tbody","td","textarea","tfoot","th","thead","time","title","tr","track","u","ul","var","video","wbr","abbr","object"];
 
